8ee590100e4795d466abab5155fb7400c3a901ec
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-31 12:47:48 -0700

    Update AsciiChars.java
    Finished AsciiChars class (I think)

--- AsciiChars.java
+++ AsciiChars.java
@@ -1,1 +1,26 @@
+public class AsciiChars 
+{
+  public static void printNumbers()
+  {
+	  //ASCII #48-57 = digits
+	  for(int c = 48; c < 58; ++c) {
+		  System.out.println((char)c);
+	  }
+  }
 
+  public static void printLowerCase()
+  {
+	  //ASCII #97-122 = lowercase letters
+	  for(int c = 97; c < 123; ++c) {
+		  System.out.println((char)c);
+	  }
+  }
+
+  public static void printUpperCase()
+  {
+	  //ASCII #65-90 = uppercase letters
+	  for(int c = 65; c < 91; ++c) {
+		  System.out.println((char)c);
+	  }
+  }
+}//end of AsciiChars class
