db2af07f19102a0ad0adf751fe253369298a7178
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-31 13:07:46 -0700

    Update AsciiChars.java
    Updated so last chars on each line don't have commas after them

--- AsciiChars.java
+++ AsciiChars.java
@@ -3,9 +3,13 @@ public class AsciiChars
   public static void printNumbers()
   {
 	  //ASCII #48-57 = digits
-	  for(int c = 48; c < 58; ++c) {
+	  for(int c = 48; c < 57; ++c) {
 		  System.out.print((char)c + ", ");
 	  }
+	  //prints last char without comma after it
+	  int c = 57;
+	  System.out.print((char)c);
+	  
 	  //creates some space between sections
 	  System.out.println("\n");
   }
@@ -13,9 +17,13 @@ public class AsciiChars
   public static void printLowerCase()
   {
 	  //ASCII #97-122 = lowercase letters
-	  for(int c = 97; c < 123; ++c) {
+	  for(int c = 97; c < 122; ++c) {
 		  System.out.print((char)c + ", ");
 	  }
+	  //prints last char without comma after it
+	  int c = 122;
+	  System.out.print((char)c);
+	  
 	  //creates some space between sections
 	  System.out.println("\n");
   }
@@ -23,9 +31,13 @@ public class AsciiChars
   public static void printUpperCase()
   {
 	  //ASCII #65-90 = uppercase letters
-	  for(int c = 65; c < 91; ++c) {
+	  for(int c = 65; c < 90; ++c) {
 		  System.out.print((char)c + ", ");
 	  }
+	  //prints last char without comma after it
+	  int c = 90;
+	  System.out.print((char)c);
+	  
 	  //creates some space between sections
 	  System.out.println("\n");
   }
