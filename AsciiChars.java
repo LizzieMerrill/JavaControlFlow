901cba23e5f70fcfbd63b63de8573d05f0519e94
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-31 12:54:37 -0700

    Update AsciiChars.java
    Updated class to take up less printing space on the console

--- AsciiChars.java
+++ AsciiChars.java
@@ -4,23 +4,29 @@ public class AsciiChars
   {
 	  //ASCII #48-57 = digits
 	  for(int c = 48; c < 58; ++c) {
-		  System.out.println((char)c);
+		  System.out.print((char)c + ", ");
 	  }
+	  //creates some space between sections
+	  System.out.println("\n\n");
   }
 
   public static void printLowerCase()
   {
 	  //ASCII #97-122 = lowercase letters
 	  for(int c = 97; c < 123; ++c) {
-		  System.out.println((char)c);
+		  System.out.print((char)c + ", ");
 	  }
+	  //creates some space between sections
+	  System.out.println("\n\n");
   }
 
   public static void printUpperCase()
   {
 	  //ASCII #65-90 = uppercase letters
 	  for(int c = 65; c < 91; ++c) {
-		  System.out.println((char)c);
+		  System.out.print((char)c + ", ");
 	  }
+	  //creates some space between sections
+	  System.out.println("\n\n");
   }
 }//end of AsciiChars class
